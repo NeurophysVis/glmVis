c16885e8fe2fab1984d1f308e60703d6795329db
edeno <edeno@users.noreply.github.com>
2014-10-24 16:54:16 -0400

    Set scale of data dimensions

--- js/parallel-coord.js
+++ js/parallel-coord.js
@@ -2,8 +2,8 @@ var margin = {top: 30, right: 10, bottom: 10, left: 10},
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
-var x = d3.scale.ordinal().rangePoints([0, width], 1),
-    y = {};
+var xScale = d3.scale.ordinal().rangePoints([0, width], 1),
+    yScale = {};
 
 var line = d3.svg.line(),
     axis = d3.svg.axis().orient("left"),
@@ -16,10 +16,24 @@ var svg = d3.select("body").append("svg")
   .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-d3.csv("DATA/Intertrial Interval apc.csv", function(error, neurons) {
+// Load Data
+timePeriod = "Intertrial Interval";
+d3.csv("DATA/" + timePeriod + " apc.csv", function(error, neurons) {
 
+// Filter out sorting Variables
 dimensions = d3.keys(neurons[0]).filter(function(d) {
-    return !["Neurons", "Session_Name", "Wire_Number", "Unit_Number", "Brain_Area", "Monkey", "Average_Firing_Rate"].indexOf(d);
-});
+      var sortingVariables = ["Neurons", "Session_Name", "Wire_Number", "Unit_Number", "Brain_Area", "Monkey", "Average_Firing_Rate"];
+      return sortingVariables.indexOf(d) == -1;
+    });
 
-};
+// Set xScale domain
+xScale.domain(dimensions);
+
+// Set yScale domain and range by looping over each data dimension and getting its max and min
+dimensions.map(function(d) {
+  yScale[d] = d3.scale.linear()
+        .domain(d3.extent(neurons, function(p) { return +p[d]; }))
+        .range([height, 0]);
+  });
+
+});
