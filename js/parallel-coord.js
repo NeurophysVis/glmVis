c6243d778aaf9da4212a0cd13bb8dbcf4f28ede4
edeno <edeno@users.noreply.github.com>
2014-10-28 17:56:41 -0400

    Removed grid lines

--- js/parallel-coord.js
+++ js/parallel-coord.js
@@ -1,20 +1,20 @@
 var margin = {top: 30, right: 10, bottom: 10, left: 205},
-    width = 700 - margin.left - margin.right,
-    height = 700 - margin.top - margin.bottom;
+width = 700 - margin.left - margin.right,
+height = 700 - margin.top - margin.bottom;
 
 var yScale = d3.scale.ordinal().rangePoints([height, 0], 1),
-    xScale = {};
+xScale = {};
 
 var line = d3.svg.line(),
-    axis = d3.svg.axis().orient("left"),
-    background,
-    foreground;
+axis = d3.svg.axis().orient("left"),
+background,
+foreground;
 
 var svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+.attr("width", width + margin.left + margin.right)
+.attr("height", height + margin.top + margin.bottom)
+.append("g")
+.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 // Load Data
 timePeriod = "Intertrial Interval";
@@ -22,9 +22,9 @@ d3.csv("DATA/" + timePeriod + " apc.csv", function(error, neurons) {
 
   // Filter out sorting Variables
   dimensions = d3.keys(neurons[0]).filter(function(dim) {
-      var sortingVariables = ["Neurons", "Session_Name", "Wire_Number", "Unit_Number", "Brain_Area", "Monkey", "Average_Firing_Rate"];
-      return sortingVariables.indexOf(dim) == -1;
-    });
+    var sortingVariables = ["Neurons", "Session_Name", "Wire_Number", "Unit_Number", "Brain_Area", "Monkey", "Average_Firing_Rate"];
+    return sortingVariables.indexOf(dim) == -1;
+  });
 
   // Reverse dimension order for better understandability.
   dimensions = dimensions.reverse();
@@ -34,45 +34,48 @@ d3.csv("DATA/" + timePeriod + " apc.csv", function(error, neurons) {
 
   // Set xScale domain and range by looping over each data dimension and getting its max and min
   xMin = d3.min(dimensions.map(function(dim) {
-        return d3.min(neurons, function(neuron) { return +neuron[dim]; });
+    return d3.min(neurons, function(neuron) { return +neuron[dim]; });
   }));
 
   xMax = d3.max(dimensions.map(function(dim) {
-          return d3.max(neurons, function(neuron) { return +neuron[dim]; });
-    }));
+    return d3.max(neurons, function(neuron) { return +neuron[dim]; });
+  }));
 
   // Set xScale for each dimension
-  dimensions.filter(function(dim) {
+  dimensions.map(function(dim) {
     xScale[dim] = d3.scale.linear().domain([xMin, xMax]).range([0, width]);
-    return;
   });
 
-  // Add grey background lines for context.
- background = svg.append("g")
-     .attr("class", "background")
-   .selectAll("path")
-     .data(neurons)
-   .enter().append("path")
-     .attr("d", path);
+var brain_areas = d3.nest()
+    .key(function(d) { return d["Brain_Area"]; })
+    .entries(neurons);
+
+  // // Add grey background lines for context.
+  // background = svg.append("g")
+  // .attr("class", "background")
+  // .selectAll("path")
+  // .data(neurons)
+  // .enter().append("path")
+  // .attr("d", path);
 
   // Add blue foreground lines for focus.
   foreground = svg.append("g")
-      .attr("class", "foreground")
-    .selectAll("path")
-      .data(neurons)
-    .enter().append("path")
-      .attr("d", path);
+  .attr("class", "foreground")
+  .selectAll("path")
+  .data(neurons)
+  .enter().append("path")
+  .attr("d", path);
 
   // Add a group element for each dimension.
   var g = svg.selectAll(".dimension")
-      .data(dimensions)
-    .enter().append("g")
-      .attr("class", "dimension")
-      .attr("transform", function(d) { return "translate(0," + yScale(d) + ")"; });
+  .data(dimensions)
+  .enter().append("g")
+  .attr("class", "dimension")
+  .attr("transform", function(d) { return "translate(0," + yScale(d) + ")"; });
 
   // Add an axis and title.
   g.append("g")
-      .attr("class", "axis")
+  .attr("class", "grid")
       .each(function(dim) { d3.select(this).call(makeXAxis(dim)); })
     .append("text")
       .style("text-anchor", "end")
@@ -82,26 +85,33 @@ d3.csv("DATA/" + timePeriod + " apc.csv", function(error, neurons) {
 
   // Add and store a brush for each axis.
   g.append("g")
-      .attr("class", "brush")
-      .each(function(dim) {
-          d3.select(this).call(makeXBrush(dim));
-        })
-    .selectAll("rect")
-      .attr("y", -8)
-      .attr("height", 16);
+  .attr("class", "brush")
+  .each(function(dim) {
+    d3.select(this).call(makeXBrush(dim));
+  })
+  .selectAll("rect")
+  .attr("y", -8)
+  .attr("height", 16);
+
+  //
+  // g.selectAll(".grid")
+  // .on("mouseover", function(d){
+  //     d3.select(this).style("opacity", 0.0)
+  //   });
+
 });
 
 // Returns the path for a given data point.
 function path(data_point) {
   return line(dimensions.map(function(dim) {
     return [xScale[dim](data_point[dim]), yScale(dim)];
-    }));
+  }));
 }
 
 // Handles a brush event, toggling the display of foreground lines.
 function brush() {
   var actives = dimensions.filter(function(dim) { return !xScale[dim].brush.empty(); }),
-      extents = actives.map(function(dim) { return xScale[dim].brush.extent(); });
+  extents = actives.map(function(dim) { return xScale[dim].brush.extent(); });
   foreground.style("display", function(neuron) {
     return actives.every(function(active_dim, extent_ind) {
       return (extents[extent_ind][0] <= neuron[active_dim]) && (neuron[active_dim] <= extents[extent_ind][1]);
@@ -111,18 +121,16 @@ function brush() {
 // Creates the x-axis for a given dimension
 function makeXAxis(dim) {
   var newAxis = axis
-        .scale(xScale[dim])
-        .orient("top");
+  .scale(xScale[dim])
+  .orient("top")
+  .tickSize(0, 0, 0);
 
   if (dim.indexOf("Rule") == -1) {
     newAxis
-        .ticks(0)
-        .tickSize(0, 0, 0);
+    .ticks(0);
   } else {
     newAxis
-        .ticks(5)
-        .tickSize(-height, 0, 0);
-
+    .ticks(5)
   }
 
   return newAxis;
